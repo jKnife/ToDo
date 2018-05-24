e7a54f26231a093c0794e436e76d3f99d3434c78
KaGacz <kagacz3@gmail.com>
2018-05-24 21:33:08 +0200

    Implement view pager

--- app/src/main/java/com/iknife/todo/MainActivity.kt
+++ app/src/main/java/com/iknife/todo/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.iknife.todo
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        Log.e("TEST", "Fragment")
+
+        viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
     }
+
+    class ViewPagerAdapter(manager: FragmentManager) : FragmentStatePagerAdapter(manager){
+        override fun getItem(position: Int): Fragment {
+            return Page()
+        }
+
+        override fun getCount(): Int {
+            return 2
+        }
+    }
+
+
 }
