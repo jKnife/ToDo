f66520354f70ae11e5d250f680281da0da324c7c
KaGacz <kagacz3@gmail.com>
2018-05-25 16:32:35 +0200

    Merge feature/add_task

---
+++ app/src/main/java/com/iknife/todo/CreateTaskActivity.kt
@@ -0,0 +1,14 @@
+package com.iknife.todo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CreateTaskActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_task)}
+
+
+
+}
--- app/src/main/java/com/iknife/todo/MainActivity.kt
+++ app/src/main/java/com/iknife/todo/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.iknife.todo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import com.github.clans.fab.FloatingActionMenu
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 import android.support.v7.widget.LinearLayoutManager
+import com.github.clans.fab.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,5 +30,18 @@ class MainActivity : AppCompatActivity() {
         adapter = TaskListAdapter(tasksList)
 
         task_list.adapter = adapter
+
+        val fabTask : FloatingActionButton = floatingActionItemTask
+        fabTask.setOnClickListener { view ->
+            val intent = Intent(this, CreateTaskActivity::class.java)
+            startActivity(intent)
+        }
+
+        val fabProject : FloatingActionButton = floatingActionItemProject
+        fabProject.setOnClickListener { view ->
+            val intent = Intent(this, CreateProjectActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
