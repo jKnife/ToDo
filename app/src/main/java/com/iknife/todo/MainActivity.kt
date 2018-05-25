c89af11e3224f4d6b40c66babe4c9488ab53a682
KaGacz <kagacz3@gmail.com>
2018-05-25 13:12:47 +0200

    Assign layout manager to task_list

--- app/src/main/java/com/iknife/todo/MainActivity.kt
+++ app/src/main/java/com/iknife/todo/MainActivity.kt
@@ -2,11 +2,19 @@ package com.iknife.todo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        linearLayoutManager = LinearLayoutManager(this)
+
+        task_list.layoutManager = linearLayoutManager
     }
 }
